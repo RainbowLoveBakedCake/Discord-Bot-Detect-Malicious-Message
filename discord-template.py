0217afc65aa0fdbdf4128abb36a4d23edfb99d48
RainbowLoveBakedCake <ArmadeissMK02@gmail.com>
2022-06-19 20:43:29 +0700

    practice bot coding

--- discord-template.py
+++ discord-template.py
@@ -9,15 +9,31 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
 #connect client
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
+#    for guild in client.guilds:
+#        if guild.name == GUILD:
+#            break
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(
         f'{client.user} is connected to following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+    members = '\n -'.join([member.name for member in guild.members])
+    printf(f'Guild Members:\n - {members}' )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my discord server!'
+    )
+
+client = CustomClient()
 client.run(TOKEN)
