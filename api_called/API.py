b4a3a0c84f32f1cbf42bfb0aaa04ed0c3cf8a3c6
RainbowBakedCake <44669371+RainbowLoveBakedCake@users.noreply.github.com>
2022-08-18 21:38:29 +0700

    Update API.py
    Updated remove www in Discord

--- api_called/API.py
+++ api_called/API.py
@@ -2,9 +2,17 @@ from urllib import response
 import requests
 import json
 import base64
+import re
+from urllib.parse import urlparse
 
 def api_scanurl(message):
     target_url = message
+    if target_url.find('www'):
+        target_url = re.sub(r'www.','',target_url)
+        
+        #uncomment below for debugging purpose only
+        #print("target url", target_url)
+
     url_id = base64.urlsafe_b64encode(target_url.encode()).decode().strip("=")
     url = "https://www.virustotal.com/api/v3/urls/" + url_id
 
@@ -16,11 +24,18 @@ def api_scanurl(message):
 
     response = requests.request("GET", url, headers=headers)
     decodedResponse = json.loads(response.text)
+    
+    #uncomment below for debugging purpose only
+    #print(decodedResponse)
 
     malicious_score = (decodedResponse["data"]["attributes"]["last_analysis_stats"]["malicious"])
     return malicious_score
 
+    ##test case to check malicious score
     # if malicious_score > 0 :
     #     print(malicious_score, 'Malicious URL')
     # else:
     #     print('URL Safe')
+
+#test case
+#print(api_scanurl("https://www.discord-login.ru/discord"))
