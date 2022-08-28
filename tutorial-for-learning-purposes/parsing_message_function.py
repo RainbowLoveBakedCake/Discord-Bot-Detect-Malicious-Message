064ee3792fa906a3149bc90e8ddaaf4916b26c3b
RainbowBakedCake <44669371+RainbowLoveBakedCake@users.noreply.github.com>
2022-08-28 21:57:29 +0700

    Update parsing_message_function.py

--- tutorial-for-learning-purposes/parsing_message_function.py
+++ tutorial-for-learning-purposes/parsing_message_function.py
@@ -3,7 +3,6 @@ import re
 def parsing_message(message):
     for line in message:
         urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line)
-        #print(urls)
     new_url = ''
     new_url += urls[0]
     return new_url
