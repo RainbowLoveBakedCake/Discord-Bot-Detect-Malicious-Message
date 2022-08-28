b92e275f701380d82d32038b74eedddbdaae9a9b
RainbowBakedCake <44669371+RainbowLoveBakedCake@users.noreply.github.com>
2022-08-28 22:01:39 +0700

    Update discord-template.py
    Cleaning the code

--- discord-template.py
+++ discord-template.py
@@ -15,27 +15,17 @@ def find_domain(url):
 
 #parsing message
 def parsing_message(message):
-    urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', message)
-    
-    #uncomment below for debugging purposes only
-    #print(urls)
-    
+    urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', message) 
     return urls
 
 #connect to machine learning
 def machine_learning_function(parsed_message):
     ml_parsed_message = parsed_message
     
-    #uncomment below for debugging purposes only
-    #print("ml_parsed_message: ", ml_parsed_message)
-    
     #for taking domain-only to be checked by machine learning
     machinelearning_domain_only = ' '.join(map(str, ml_parsed_message))
     ml_final_predict = find_domain(machinelearning_domain_only)
     ml_final_predict = [ml_final_predict]
-    
-    #uncomment below for debugging purposes only
-    #print("ml_final_predict : ", ml_final_predict)
     
     labelEncoder = LabelEncoder()
     
@@ -60,7 +50,7 @@ def api_vt_function(parsed_message):
     return detect_url
 
 #token is from Discord Developer Portal > Bot > you will generate token, just copy and put it here
-TOKEN = 'OTg4MDYxNDcxODQ4MTQ0OTI2.Gmk0uu.pYSYTnONNCQFzPxtnSdKV2vWJ_Dp8qlDpylSKI'
+TOKEN = 'DISCORD-API-KEY'
 client = discord.Client()
 
 #turn on your discord bot
@@ -89,31 +79,15 @@ async def on_message(message):
     parsed_message = parsing_message(user_message)
     url_detect_beginning = machine_learning_function(parsed_message)
     
-    
-    #uncomment below for debugging purposes only
-    #print("parsed_message: ", parsed_message)
-
-    #because api just accept string 
+    #because API just accept string 
     new_parsed_message = ' '.join(map(str, parsed_message))
-    
-    #uncomment below for debugging purposes only
-    #print("new_parsed_message: ", new_parsed_message)
    
    #if machine learning detect this malicious
     if url_detect_beginning == 1:
         #checking again with virus total
         url_detect_final = api_vt_function(new_parsed_message)
         print("new_parsed_message: ", new_parsed_message)
-        if url_detect_final + url_detect_beginning > 1:
-            
-            ##uncomment below for debugging this bot
-            #print("user_message: ",user_message)
-            #print("type of user_message:", type(user_message))
-            #print("url detect beginning:",url_detect_beginning)
-            #print("type of user_detect_beginning:", type(url_detect_beginning))
-            #print("url detect final:",url_detect_final)
-            #print("type of url_detect_final:", type(url_detect_final))
-            
+        if url_detect_final + url_detect_beginning > 1:    
             #delete message
             await message.delete()
             await message.channel.send("Message from "+ str(message.author) +" has been deleted due to trying sending malicious messages")
