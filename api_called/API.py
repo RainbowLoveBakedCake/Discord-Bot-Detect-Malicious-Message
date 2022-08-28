3e1cc44779fde0ec3719d87f43a2bc453ca6566a
RainbowBakedCake <44669371+RainbowLoveBakedCake@users.noreply.github.com>
2022-08-28 21:55:31 +0700

    Update API.py

--- api_called/API.py
+++ api_called/API.py
@@ -9,9 +9,6 @@ def api_scanurl(message):
     target_url = message
     if target_url.find('www'):
         target_url = re.sub(r'www.','',target_url)
-        
-        #uncomment below for debugging purpose only
-        #print("target url", target_url)
 
     url_id = base64.urlsafe_b64encode(target_url.encode()).decode().strip("=")
     url = "https://www.virustotal.com/api/v3/urls/" + url_id
@@ -30,12 +27,3 @@ def api_scanurl(message):
 
     malicious_score = (decodedResponse["data"]["attributes"]["last_analysis_stats"]["malicious"])
     return malicious_score
-
-    ##test case to check malicious score
-    # if malicious_score > 0 :
-    #     print(malicious_score, 'Malicious URL')
-    # else:
-    #     print('URL Safe')
-
-#test case
-#print(api_scanurl("https://www.discord-login.ru/discord"))
